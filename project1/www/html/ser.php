0213643fd3d9dab8a97b4797672aa0df3d17026e
groveswallow <395267141@qq.com>
2019-11-14 08:56:36 +0800

    20191114

--- project1/www/html/ser.php
+++ project1/www/html/ser.php
@@ -1,34 +1,154 @@
 <?php
-  $host="localhost";
-  $usr="lcbb3734";
-  $password=3734;
-  $db="lcbb3734";
-  $con=mysqli_connect($servername, $username, $password,$db);
+  $host = "localhost";
+  $usr = "lcbb3734";
+  $password = 734;
+  $db = "lcbb3734";
+  $con = mysqli_connect($servername, $username, $password,$db);
+  $TF = $_REQUEST['TF'];
+  $Gen = $_REQUEST['Gen'];
+  $Cancer = $_REQUEST['Cancer'];
+  $flag = 0;
+
   function sertf($TF,$con){
-    $sql = "select * from lcbb3734 where tf like $TF";
+    $index = 0;
+    $sql = "select * from brca_v1 where tf like $TF";
     $result = mysqli_query($con, $sql);
-    echo "<h1>$TF</h1>";
+    $r_number = mysqli_num_rows($result);
+    echo "<h1><span class='glyphicon glyphicon-search' aria-hidden='true' style='font-size：30px;'></span>The Result of Search</h1>";
+    echo "<hr>";
+    if($r_number == 0){
+      echo "<h2>We don not find anything.</h2> ";
+    }
+    else{
+      echo "<div class='table'>
+      <p>There are ".$r_number." results about <b>".$TF."</b></p>
+      <table class='table table-hover table-bordered'>
+        <thead>
+          <tr>
+            <th>TF</th>
+            <th>gene</th>
+            <th>Characteristics</th>
+            <th>Regulation_type</th>
+            <th>Cancer_hallmark</th>
+            <th>Details</th>
+          </tr>
+        </thead>";
+    while($row = mysqli_fetch_assoc($result))
+     {
+      $index = $index + 1;
+      echo "<tbody>";
+      echo "<td><b>".$row['tf']."</b></td>";
+      echo "<td>".$row['gene']."</td>";
+      // echo "<td>".$row['cancer_name']."</td>";
+      echo "<td>".$row['characteristics']."</td>";
+      echo "<td>".$row['regulation_type']."</td>";
+      echo "<td>".$row['cancer_hallmark']."</td>";
+      echo "<td><a id='det'>details.....</a></td>";
+      echo "</tbody>";
+    }
+    echo "</tbody></table></div>";
+    }
   }
+
   function sergen($Gen,$con){
-    echo "<h1>$Gen</h1>";
+    $sql = "select * from b这是一段测试用的文本，没有实际意义，只是想达到显示效果。rca_v1 where gene like $Gen";
+    $result = mysqli_query($con, $sql);
+    $r_number = mysqli_num_rows($result);
+    echo "<h1><span class='glyphicon glyphicon-search' aria-hidden='true' style='font-size：30px;'></span>The Result of Search</h1>";
+    echo "<hr>";
+    if($r_number == 0){
+      echo "<h2>We don not find anything.</h2> ";
+    }
+    else{
+      echo "<div class='table'>
+      <p>There are ".$r_number." results about <b>".$Gen."</b></p>
+      <table class='table table-hover table-bordered'>
+        <thead>
+          <tr>
+            <th>TF</th>
+            <th>gene</th>
+            <th>Characteristics</th>
+            <th>Regulation_type</th>
+            <th>Cancer_hallmark</th>
+            <th>Details</th>
+          </tr>
+        </thead>";
+    while($row = mysqli_fetch_assoc($result))
+     {
+      echo "<tbody>";
+      echo "<td><b>".$row['gene']."</b></td>";
+      echo "<td>".$row['tf']."</td>";
+      // echo "<td>".$row['cancer_name']."</td>";
+      echo "<td>".$row['characteristics']."</td>";
+      echo "<td>".$row['regulation_type']."</td>";
+      echo "<td>".$row['cancer_hallmark']."</td>";
+      echo "<td><a id='det'>details.....</a></td>";
+      echo "</tbody>";
+    }
+    echo "</tbody></table></div>";
+    }
   }
-  function serpmid($PMID,$con){
-    echo "<h1>$PMID</h1>";
+  function serpmid($Cancer,$con){
+    $sql = "select * from brca_v1 where cancer like $Cancer";
+    $result = mysqli_query($con, $sql);
+    $r_number = mysqli_num_rows($result);
+    echo "<h1><span class='glyphicon glyphicon-search' aria-hidden='true' style='font-size：30px;'></span>The Result of Search</h1>";
+    echo "<hr>";
+    if($r_number == 0){
+      echo "<h2>We don not find anything.</h2> ";
+    }
+    else{
+      echo "<div class='table'>
+      <p>There are ".$r_number." results about <b>".$Cancer."</b></p>
+      <table class='table table-hover table-bordered'>
+        <thead>
+          <tr>
+            <th>TF</th>
+            <th>gene</th>
+            <th>Characteristics</th>
+            <th>Regulation_type</th>
+            <th>Cancer_hallmark</th>
+            <th>Details</th>
+          </tr>
+        </thead>";
+    while($row = mysqli_fetch_assoc($result))
+     {
+      echo "<tbody>";
+      // echo "<td><b>".$row['cancer_name']."</b></td>";
+      echo "<td>".$row['tf']."</td>";
+      echo "<td>".$row['gene']."</td>";
+      echo "<td>".$row['characteristics']."</td>";
+      echo "<td>".$row['regulation_type']."</td>";
+      echo "<td>".$row['cancer_hallmark']."</td>";
+      echo "<td><a id='det'>details.....</a></td>";
+      echo "</tbody>";
+    }
+    echo "</tbody></table></div>";
+    }
   }
   if (!$con){
     echo "<h1>数据库连接失败，请联系管理员：XXXXXXXXXX</h1>";
+    $flag = 1;
   }
-  $TF = $_REQUEST['TF'];
-  $Gen = $_REQUEST['Gen'];
-  $PMID = $_REQUEST['PMID'];
-  if($TF){
-    sertf($TF,$con);
+  else{  
+    if($TF){
+      sertf($TF,$con);
   }
   elseif($Gen){
     sergen($Gen,$con);
   }
   else {
-    serpmid($PMID,$con);
+    serpmid($Cancer,$con);
   }
+if($flag == 0){
+  echo " <script>
+  $(function () {
+    $('a#det').on('click', (function () {
+      $(this).parent().children('td').eq(0).text();
+    }))
+  })
+</script>";
+}
+}
   mysqli_close($con);
 ?>
---
+++ project1/www/html/test.php
@@ -0,0 +1,6 @@
+<?php 
+$pp = 1 ;
+echo "s = ".$pp."\n";
+echo "'$pp'\n";
+echo "'t$pp'"
+?>
