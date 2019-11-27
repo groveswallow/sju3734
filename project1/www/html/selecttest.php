2209e14e91a24a37d1517fdc480c3f63a81aa2be
groveswallow <395267141@qq.com>
2019-11-27 17:35:41 +0800

    update

--- project1/www/html/selecttest.php
+++ project1/www/html/selecttest.php
@@ -4,6 +4,7 @@ $con = mysqli_connect($host, $usr, $password,$db);
 $sql = "show tables";
 $result = mysqli_query($con,$sql);
 $row = mysqli_fetch_assoc($result);
+echo json_encode($row);
 while($row){
 echo "<option value='$row[0]'>$row[0]</option>";
 $row = mysqli_fetch_assoc($result);
