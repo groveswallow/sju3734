867d36ec32a0230090aa2bdb375ba7e03ba34f63
groveswallow <395267141@qq.com>
2019-11-29 15:48:44 +0800

    update

--- project1/www/html/lib/now-addtest.py
+++ project1/www/html/lib/now-addtest.py
@@ -2,8 +2,9 @@
 @Author: Tang
 @Date: 2019-11-26 18:03:59
 @LastEditors: Tang
-@LastEditTime: 2019-11-27 14:32:59
+@LastEditTime: 2019-11-28 18:44:05
 @Description: 
+待解决：1.未解决33种肿瘤数据表的导入问题。2.PMID为空的时候，与上一行PMID一样，title与原文按上一行填充。（绝对的）          在该条件下，假如tf为空的时候，该格与上一行一样。  只有PMID号、原文、标题，其他列无数据，丢弃。
 '''
 import openpyxl
 import sys
@@ -12,7 +13,7 @@ import re
 import MySQLdb as db
 
 
-def add(path):
+def addexcel(path):
     host = "localhost"
     usr = "lcbb3734"
     password = "3734"
@@ -20,7 +21,7 @@ def add(path):
     con = db.connect(host, usr, password, database,
                      use_unicode=True, charset="utf8")  # 切记检查编码以及添加编码。
     cur = con.cursor()
-    file_dir = os.listdir(path) #遍历该目录下的所有文件，文件名以数组的形式储存在file_dir中
+    file_dir = os.listdir(path)  # 遍历该目录下的所有文件，文件名以数组的形式储存在file_dir中
     for i in file_dir:
         filepath = os.path.join(path, i)
         wb = openpyxl.load_workbook(filepath)
@@ -34,11 +35,12 @@ def add(path):
                 VALUES ('%s','%s');""" % (i[0:-5], ws['A%d' % x].value, ws['B%d' % x].value)
                 cur.execute(insql)
         else:
-            pass  # 这里是33个表的写入程序
+            cresql = """CREATE TABLE %s(
+                BriCancer CHAR(10) NOT NULL,LongCancer CHAR(100) NOT NULL);""" % i[0:-5]  # 这里是33个表的写入程序
     con.commit()
     con.close()
 
 
 if __name__ == "__main__":
     path = sys.argv[1]
-    add(path)
+    addexcel(path)
